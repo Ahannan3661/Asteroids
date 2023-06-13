4633a90122b7ae03e6a0579ad4ad2df01c26607c
Ahannan3661 <ranaabdulhannan3661@gmail.com>
2023-06-14 01:41:27 +0500

    Final Commit

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// The HUD
+/// </summary>
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    Text Timer;
+    [SerializeField]
+    Text scoreText;
+
+    int score;
+    const string ScorePrefix = "Score: ";
+    // game timer support
+    float elapsedSeconds = 0;
+    bool running = true;
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+        scoreText.text = ScorePrefix + score+"";
+        Timer.text = "0";
+    }
+
+
+    public void AddPoints(int points)
+    {
+        score += points;
+        scoreText.text = ScorePrefix + score + "";
+    }
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+        if (running)
+        {
+            elapsedSeconds += Time.deltaTime;
+            Timer.text = ((int)elapsedSeconds).ToString();
+        }
+	}
+
+    /// <summary>
+    ///  Stops the game timer
+    /// </summary>
+    public void StopGameTimer()
+    {
+        running = false;
+    }
+}
